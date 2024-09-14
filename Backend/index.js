8b120772608735bdeacfa7773070f011664b6756
Lamisa-51 <122662374+Lamisa-51@users.noreply.github.com>
2024-09-14 07:21:22 -0700

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,14 @@ import cors from "cors";
 import courseRoute from "./route/course.route.js"
 import userRoute from "./route/user.route.js"
 const app = express();
-app.use(cors());
+app.use(cors(
+    (
+        origin:("skillbridge-api.vercel.app"),
+        methods:("POST","GET"),
+        credentials:true
+    )
+
+));
 app.use(express.json())
 
 dotenv.config();
