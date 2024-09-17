1b171b367507740c26b514767aff84bc94044b88
Lamisa-51 <122662374+Lamisa-51@users.noreply.github.com>
2024-09-17 08:55:18 -0700

    Update course.controller.js

--- Backend/controller/course.controller.js
+++ Backend/controller/course.controller.js
@@ -1,11 +1,13 @@
-import Course from "../model/course.model.js";
+export Course from "../model/course.model.js";
 
 export const getCourse = async(req, res) => {
     try {
         const course = await Course.find({});
         res.status(200).json(courses);
     } catch (error) {
-        console.log("Error: ", error);
-        res.status(500).json(error);
+       console.error("Error fetching courses:", error);
+    res.status(500).json({ message: "Server Error", error });
     }
 };
+
+
