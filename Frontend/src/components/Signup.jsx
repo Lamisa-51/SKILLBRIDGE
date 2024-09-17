84d06cfd42a50b5c80c6d2085d9c9f89b731efbd
Lamisa-51 <122662374+Lamisa-51@users.noreply.github.com>
2024-09-16 23:36:01 -0700

    Update Signup.jsx

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -27,7 +27,7 @@ function Signup() {
       password: data.password,
     };
     await axios
-      .post("http://skillbridge-api.vercel.app/user/signup", userInfo)
+      .post("https://skillbridge-api.vercel.app/user/signup", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
