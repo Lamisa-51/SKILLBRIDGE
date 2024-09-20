cbf9ae5216cfae55e31674c40ad8f69ecb3d6598
Lamisa-51 <122662374+Lamisa-51@users.noreply.github.com>
2024-09-19 23:01:45 -0700

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,27 +1,17 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AuthProvider from "./context/AuthProvider.jsx";
 import App from "./App.jsx";
 import "./index.css";
-import AuthProvider from "./context/AuthProvider.jsx";
 
 
-const router = createBrowserRouter([
-  {
-    path: "/*",
-    element: (
-      <AuthProvider>
-        <div className="dark:bg-slate-900 dark:text-white">
-          <App />
-        </div>
-      </AuthProvider>
-    ),
-  },
-]);
+
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
+  <React.StrictMode><AuthProvider><App />
+  </AuthProvider>
+
   </React.StrictMode>
 );
+
